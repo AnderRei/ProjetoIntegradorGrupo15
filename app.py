7ee120f690536f9f4be8be8ede59e643bebfc498
gustavo davi <gdp.strategic@gmail.com>
2026-04-26 18:23:34 -0300

    tratamento de dados, criação de tipo_produto e dataset tratado

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from src.tratamento import carregar_e_tratar
 
-st.title("Projeto de Análise da Amazon")
+st.title("Dashboard - Produtos")
 
-st.write(" em construçã Dashboard...")
-st.write ("Aguardando arquivos tratados para inicar construção do dashboard")
+df = carregar_e_tratar()
+
+st.subheader("Dados tratados")
+st.write(df)
+
+st.subheader("")
